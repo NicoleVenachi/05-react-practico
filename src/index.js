c04f439fc7be67f355a575497327196548b4fb90
nicolevenachi <ludwing@unicauca.edu.co>
2023-03-02 13:06:56 -0500

    maquetacion y router-dom

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './components/App.jsx';
+
+
+//rnder en rect@17
+ReactDOM.render(<App />, document.getElementById('app'));
+
