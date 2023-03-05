feafae5b67347080b312913846aba5ef1e93f36f
nicolevenachi <ludwing@unicauca.edu.co>
2023-03-04 19:39:42 -0500

    todas las vistas, maquetadas

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Header } from '../components/Header';
 
-function Layout({children}) {
+function Layout({ children }) {
   return (
     <div className='Layout'>
+      <Header />
       {children}
     </div>
   )
 }
 
-export {Layout}
+export { Layout }
