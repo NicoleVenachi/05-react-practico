42e886abe864d07be44dc177ca912fdf8728e10b
nicolevenachi <ludwing@unicauca.edu.co>
2023-03-07 21:24:44 -0500

    Hooks menu y estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+//components
+import Menu from "@components/Menu"
 
 import '@styles/Header.scss'
 
+//asets
 import menu from '@icons/icon_menu.svg';
 import logo from "@logos/logo_yard_sale.svg";
 import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 function Header() {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(prevState => !prevState)
+  }
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -38,13 +49,21 @@ function Header() {
 
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email"
+            onClick={handleToggle}
+          >platzi@example.com</li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+
+      {
+        //si toggle es true, muestra menu
+        !!toggle && <Menu />
+      }
+
     </nav>
   )
 }
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products'
+
 const ProductList = () => {
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(async () => {
+
+		try {
+			const response = await fetch(API)
+			const data = await response.json()
+
+			//paso los datos de los productos
+			setProducts(data)
+		} catch (error) {
+		}
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{
+
+					//itero entre todos los productos traidos
+					products.map((product) => (
+						<ProductItem />
+					))
+				}
+				{
+
+				}
+
 			</div>
 		</section>
 	);
